c92fc6f187c77f39d4646c66e9b2d69ecb29feba
nobinkhan <nazrulkhan.dk@gmail.com>
2022-12-21 13:49:03 +0600

    wait for db fixed

--- Accounts/account/management/commands/wait_for_db.py
+++ Accounts/account/management/commands/wait_for_db.py
@@ -6,6 +6,7 @@ from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
 
 class Command(BaseCommand):
@@ -17,16 +18,19 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                # time.sleep(3)
-                self.check(databases=['default'])
-                db_up = True
+                time.sleep(2)
+                self.check(databases=settings.DATABASES)
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 3 second...')
                 time.sleep(3)
+            except Exception as msg:
+                self.stdout.write(msg.args[0])
+            else:
+                db_up = True
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
 
 
-if __name__== "__main__":
-    hd = Command()
-    hd.handle()
+# if __name__== "__main__":
+#     hd = Command()
+#     hd.handle()
