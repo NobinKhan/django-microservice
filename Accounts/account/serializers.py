6db78d2e52029b4ca67ef92c9877a325fe9e3607
nobinkhan <nazrulkhan.dk@gmail.com>
2022-12-26 19:28:09 +0600

    account create serializer

--- Accounts/account/serializers.py
+++ Accounts/account/serializers.py
@@ -1,24 +1,57 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, Serializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.exceptions import AuthenticationFailed
 
+from .models import Profile
+
 User = get_user_model()
 
-class RegisterUserSerializer(serializers.ModelSerializer):
+
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'Name', 'Phone', 'email', 'Address', 'Total_amount','Point', 'discount', 'Membership' ,'Status', 'is_staff', 'is_superuser')
+        fields = ('username', 'email', 'phone', 'is_active', 'is_deleted')
         extra_kwargs = {
-            'password': {'write_only': True}
+            'phone': {'required': True},
+        }
+
+
+class ProfileSerializer(ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('user', 'name', 'date_of_birth', 'gender', 'photo')
+        extra_kwargs = {
+            'user': {'required': True},
+            'name': {'required': True}
+        }
+
+
+class RegisterUserSerializer(ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = Profile
+        fields = ('user', 'name',)
+        extra_kwargs = {
+            'user': {'required': True},
+            # 'name': {'required': True}
         }
     
-    def create(self, validated_data):
-        password = validated_data.pop('password', settings.SECRET_KEY)
-        instance = self.Meta.model(**validated_data)
-        if password is not None:
-            instance.set_password(password)
-        instance.save()
-        return instance
+    def is_valid(self, *, raise_exception=False):
+        self.initial_data = {
+            'user': {'phone':self.initial_data.get('phone')},
+            'name': self.initial_data.get('name'),
+        }
+        return super().is_valid(raise_exception=raise_exception)
+    
+    
+    # def validate(self, attrs):
+    #     print(f"in validation = {attrs}")
+    #     return attrs
+
+    # def save(self, **kwargs):
+    #     print(f"in save = {self.validated_data}")
+    #     return super().save(**kwargs)
+
--- Accounts/account/views.py
+++ Accounts/account/views.py
@@ -15,23 +15,13 @@ User = get_user_model()
 class Register(APIView):
     # permission_classes = [IsAuthenticated]
     def post(self, request):
-        # serializer = RegisterUserSerializer(data=request.data)
+        serializer = RegisterUserSerializer(data=request.data)
 
-        # try:
-        #     is_user_available = User.objects.get(Phone = request.data["Phone"])
-        # except:
-        #     is_user_available = None
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
+        new_user = serializer.save()
+            
         
-        # if is_user_available and is_user_available.is_active == True:
-        #     return Response({"message": "user with this Phone already exists."}, status=status.HTTP_403_FORBIDDEN)
-
-        # elif is_user_available and is_user_available.is_active == False:
-        #     is_user_available.is_active = True
-        #     is_user_available.save()
-        #     return Response({"message":"User successfully registered, please verify OTP."}, status=status.HTTP_201_CREATED)
-        
-        return Response({"message":f"{request.data}"}, status=status.HTTP_201_CREATED)
-        if serializer.is_valid():
-            new_user = serializer.save()
-            if new_user:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message":f"{new_user.id}"}, status=status.HTTP_201_CREATED)
+            # if new_user:
+            #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
