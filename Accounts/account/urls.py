c5f9434834c24bfa0cc669f4e35a0f51941884d0
Md. Nazrul Islam Khan <nazrulkhan.dk@gmail.com>
2022-12-28 22:15:58 +0600

    fixed login url name

--- Accounts/account/urls.py
+++ Accounts/account/urls.py
@@ -10,8 +10,8 @@ from .views import Register, Login, SendOTP
 
 
 urlpatterns = [
+    path('login/', Login.as_view(), name='login'),
     path('register/', Register.as_view(), name='register'),
-    path('login/', Login.as_view(), name='get_token'),
     path('send_otp/', SendOTP.as_view(), name='send_otp'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
