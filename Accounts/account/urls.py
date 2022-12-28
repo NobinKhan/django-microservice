ecf5fccf5b7a189e2868b79f20a245774ed6a6ac
Md. Nazrul Islam Khan <nazrulkhan.dk@gmail.com>
2022-12-29 02:08:10 +0600

    url serial

--- Accounts/account/urls.py
+++ Accounts/account/urls.py
@@ -10,8 +10,8 @@ from .views import Register, Login, SendOTP
 
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
-    path('register/', Register.as_view(), name='register'),
     path('send_otp/', SendOTP.as_view(), name='send_otp'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('register/', Register.as_view(), name='register'),
+    path('jwt_verify/', TokenVerifyView.as_view(), name='jwt_verify'),
+    path('jwt_refresh/', TokenRefreshView.as_view(), name='jwt_refresh'),
 ]
