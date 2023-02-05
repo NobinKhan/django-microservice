a6debe04f74c12add8f12cee1cb444197f1fab4c
nobinkhan <nazrulkhan.dk@gmail.com>
2023-02-05 18:48:03 +0600

    user model fixing

--- Auth/apps/users/services.py
+++ Auth/apps/users/services.py
@@ -6,10 +6,13 @@ from apps.common.services import model_update
 from apps.users.models import BaseUser
 
 
-def user_create(
-    *, email: str, is_active: bool = True, is_admin: bool = False, password: Optional[str] = None
-) -> BaseUser:
-    user = BaseUser.objects.create_user(email=email, is_active=is_active, is_admin=is_admin, password=password)
+def user_create(*, email: str, is_active: bool = True, is_admin: bool = False, password: Optional[str] = None) -> BaseUser:
+    user = BaseUser.objects.create_user(
+        email=email, 
+        is_active=is_active, 
+        is_admin=is_admin, 
+        password=password
+    )
 
     return user
 
