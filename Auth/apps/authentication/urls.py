e204f339e5e6aa76e1d4a40bfb00b17d4ad731b4
nobinkhan <nazrulkhan.dk@gmail.com>
2023-02-08 18:40:28 +0600

    time to work on paseto token

--- Auth/apps/authentication/urls.py
+++ Auth/apps/authentication/urls.py
@@ -1,37 +1,23 @@
 from django.urls import include, path
 
 from apps.authentication.apis import (
+    UserMeApi,
     UserJwtLoginApi,
     UserJwtLogoutApi,
-    UserMeApi,
     UserSessionLoginApi,
     UserSessionLogoutApi,
 )
 
 urlpatterns = [
-    path(
-        "session/",
-        include(
-            (
-                [
-                    path("login/", UserSessionLoginApi.as_view(), name="login"),
-                    path("logout/", UserSessionLogoutApi.as_view(), name="logout"),
-                ],
-                "session",
-            )
-        ),
-    ),
-    path(
-        "jwt/",
-        include(
-            (
-                [
-                    path("login/", UserJwtLoginApi.as_view(), name="login"),
-                    path("logout/", UserJwtLogoutApi.as_view(), name="logout"),
-                ],
-                "jwt",
-            )
-        ),
-    ),
+    path("session/",include(([
+        path("login/", UserSessionLoginApi.as_view(), name="login"),
+        path("logout/", UserSessionLogoutApi.as_view(), name="logout")
+    ,],"session",)),),
+
+    path("jwt/",include(([
+        path("login/", UserJwtLoginApi.as_view(), name="login"),
+        path("logout/", UserJwtLogoutApi.as_view(), name="logout"),
+    ],"jwt",)),),
+    
     path("me/", UserMeApi.as_view(), name="me"),
 ]
