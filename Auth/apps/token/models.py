91345b963627ec90319c19c183eb3b61428de31a
nobinkhan <nazrulkhan.dk@gmail.com>
2023-02-09 18:32:56 +0600

    auth model

--- Auth/apps/token/models.py
+++ Auth/apps/token/models.py
@@ -1,14 +1,29 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 from apps.common.models import BaseModel
 from apps.token.rsa_key import generate_rsa
 
+
+# Global Variables
+User = get_user_model()
+
+
 class RsaKey(BaseModel):
-    public = models.BinaryField(verbose_name='Public Key', editable=False)
-    private = models.BinaryField(verbose_name='Private Key', editable=False)
+    public = models.BinaryField(verbose_name='Public Key', editable=False, unique=True)
+    private = models.BinaryField(verbose_name='Private Key', editable=False, unique=True)
     active = models.BooleanField(default=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["public",'private'],
+                name="unique_key_pair"
+            )
+        ]
 
     def __str__(self) -> str:
         return f"rsa_key-{self.id}"
@@ -34,4 +49,17 @@ class RsaKey(BaseModel):
 
 
 class AuthToken(BaseModel):
-    pass
+    user = models.ForeignKey(User, related_name="auth_token", on_delete=models.CASCADE, null=True, blank=True, editable=False)
+    access_token = models.BinaryField(verbose_name='Access Token', editable=False, unique=True, null=True, blank=True)
+    exp = models.DateTimeField(default=(timezone.now()+timezone.timedelta(hours=24)), editable=False)
+    is_valid = models.BooleanField(default=False, null=True, blank=True)
+    payload = models.JSONField(null=True, blank=True, editable=False)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user",'access_token'],
+                name="unique_auth_token"
+            )
+        ]
+
--- Auth/apps/token/rsa_key.py
+++ Auth/apps/token/rsa_key.py
@@ -1,58 +1,40 @@
 from cryptography.exceptions import InvalidSignature
-from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ed25519
 
-
-# def old_generate_rsa(key=None, file_name=None):
-#     rsa_private_key = rsa.generate_private_key(
-#         public_exponent=65537,
-#         key_size=2048
-#     )
-    
-#     if not key and not SECRET_KEY:
-#         key = uuid4()
-#     elif SECRET_KEY:
-#         key = SECRET_KEY
-
-#     private_key_password = bytes(key, encoding='utf-8')
-
-#     # private key
-#     private_key = rsa_private_key.private_bytes(
-#         encoding=serialization.Encoding.PEM,
-#         format=serialization.PrivateFormat.PKCS8,
-#         encryption_algorithm=serialization.BestAvailableEncryption(private_key_password)
-#     )
-
-#     # public key
-#     public_key = rsa_private_key.public_key().public_bytes(
-#     encoding=serialization.Encoding.PEM,
-#     format=serialization.PublicFormat.SubjectPublicKeyInfo
-#     )
-
-#     if file_name:
-#         private_key_file = open(f"./token/rsa/{file_name}-rsa.pem", "w")
-#         private_key_file.write(private_key.decode())
-#         private_key_file.close()
-
-#         public_key_file = open(f"./token/rsa/{file_name}-rsa.pub", "w")
-#         public_key_file.write(public_key.decode())
-#         public_key_file.close()
-    
-#     return private_key, public_key
 
 def generate_rsa(file_name=None):
     # Ed25519
 
     # Public-private key creation
-    private_key = Ed25519PrivateKey.generate()
-    public_key = private_key.public_key()
-    print(private_key.private_bytes())
-    print(type(public_key))
+    private_raw_key = ed25519.Ed25519PrivateKey.generate()
+    public_raw_key = private_raw_key.public_key()
+
+    private_key = private_raw_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+    
+    public_key = public_raw_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    if file_name:
+        private_key_file = open(f"{file_name}-rsa.pem", "w")
+        private_key_file.write(private_key.decode())
+        private_key_file.close()
+
+        public_key_file = open(f"{file_name}-rsa.pub", "w")
+        public_key_file.write(public_key.decode())
+        public_key_file.close()
     return private_key, public_key
 
 
 
 
-    '''
+'''
     # # Signature creation
     # message = b'Fundamental Cryptography in Python'
     # signature = private_key.sign(message)
@@ -94,6 +76,6 @@ def generate_rsa(file_name=None):
     # else:
     #     # Should not happen
     #     assert False
-    '''
+'''
 
 
