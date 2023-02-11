7d84274807cde86f6a5aa6d6b539e7c8e15df024
Md. Nazrul Islam Khan <nazrulkhan.dk@gmail.com>
2023-02-11 23:40:58 +0600

    token.py need to modify

--- Auth/apps/token/backends.py
+++ Auth/apps/token/backends.py
@@ -1,79 +1,22 @@
 import json
-
+import pyseto
 from datetime import timedelta
-from typing import Optional, Type, Union
+from typing import Union
 
-import jwt
+from apps.token.models import RsaKey
 from django.utils.translation import gettext_lazy as _
-from jwt import InvalidAlgorithmError, InvalidTokenError, algorithms
-
 from .exceptions import TokenBackendError
 from .utils import format_lazy
 
-try:
-    from jwt import PyJWKClient, PyJWKClientError
 
-    JWK_CLIENT_AVAILABLE = True
-except ImportError:
-    JWK_CLIENT_AVAILABLE = False
-
-ALLOWED_ALGORITHMS = {
-    "HS256",
-    "HS384",
-    "HS512",
-    "RS256",
-    "RS384",
-    "RS512",
-    "ES256",
-    "ES384",
-    "ES512",
-}
 
 
 class TokenBackend:
-    def __init__(
-        self,
-        algorithm,
-        signing_key=None,
-        verifying_key="",
-        audience=None,
-        issuer=None,
-        jwk_url: str = None,
-        leeway: Union[float, int, timedelta] = None,
-        json_encoder: Optional[Type[json.JSONEncoder]] = None,
-    ):
-        self._validate_algorithm(algorithm)
-
-        self.algorithm = algorithm
-        self.signing_key = signing_key
+    def __init__(self,verifying_key="",audience=None,issuer=None,leeway: Union[float, int, timedelta] = None,):
         self.verifying_key = verifying_key
         self.audience = audience
         self.issuer = issuer
-
-        if JWK_CLIENT_AVAILABLE:
-            self.jwks_client = PyJWKClient(jwk_url) if jwk_url else None
-        else:
-            self.jwks_client = None
-
         self.leeway = leeway
-        self.json_encoder = json_encoder
-
-    def _validate_algorithm(self, algorithm):
-        """
-        Ensure that the nominated algorithm is recognized, and that cryptography is installed for those
-        algorithms that require it
-        """
-        if algorithm not in ALLOWED_ALGORITHMS:
-            raise TokenBackendError(
-                format_lazy(_("Unrecognized algorithm type '{}'"), algorithm)
-            )
-
-        if algorithm in algorithms.requires_cryptography and not algorithms.has_crypto:
-            raise TokenBackendError(
-                format_lazy(
-                    _("You must have cryptography installed to use {}."), algorithm
-                )
-            )
 
     def get_leeway(self) -> timedelta:
         if self.leeway is None:
@@ -93,38 +36,35 @@ class TokenBackend:
             )
 
     def get_verifying_key(self, token):
-        if self.algorithm.startswith("HS"):
-            return self.signing_key
-
-        if self.jwks_client:
-            try:
-                return self.jwks_client.get_signing_key_from_jwt(token).key
-            except PyJWKClientError as ex:
-                raise TokenBackendError(_("Token is invalid or expired")) from ex
 
         return self.verifying_key
 
-    def encode(self, payload):
+    def encode(self, payload: dict):
         """
         Returns an encoded token for the given payload dictionary.
         """
+        if not bool(payload):
+            return None
+
+        # secret keys
+        rsa = RsaKey.objects.filter(active=True)
+        if rsa.count() != 1:
+            return None
+        pem_private_key = rsa.private.tobytes()
+        # pem_public_key = rsa.public.tobytes()
+
+        # generate secret key
+        secret_key = pyseto.Key.new(version=4, purpose="public", key=pem_private_key)
+
+        # get access token with payload
         jwt_payload = payload.copy()
         if self.audience is not None:
             jwt_payload["aud"] = self.audience
         if self.issuer is not None:
             jwt_payload["iss"] = self.issuer
+        access_token = pyseto.encode(secret_key, payload=jwt_payload)
 
-        token = jwt.encode(
-            jwt_payload,
-            self.signing_key,
-            algorithm=self.algorithm,
-            json_encoder=self.json_encoder,
-        )
-        if isinstance(token, bytes):
-            # For PyJWT <= 1.7.1
-            return token.decode("utf-8")
-        # For PyJWT >= 2.0.0a1
-        return token
+        return access_token
 
     def decode(self, token, verify=True):
         """
@@ -134,20 +74,31 @@ class TokenBackend:
         Raises a `TokenBackendError` if the token is malformed, if its
         signature check fails, or if its 'exp' claim indicates it has expired.
         """
-        try:
-            return jwt.decode(
-                token,
-                self.get_verifying_key(token),
-                algorithms=[self.algorithm],
-                audience=self.audience,
-                issuer=self.issuer,
-                leeway=self.get_leeway(),
-                options={
-                    "verify_aud": self.audience is not None,
-                    "verify_signature": verify,
-                },
-            )
-        except InvalidAlgorithmError as ex:
-            raise TokenBackendError(_("Invalid algorithm specified")) from ex
-        except InvalidTokenError as ex:
-            raise TokenBackendError(_("Token is invalid or expired")) from ex
+        # secret keys
+        rsa = RsaKey.objects.filter(active=True)[0]
+        # pem_private_key = rsa.private.tobytes()
+        pem_public_key = rsa.public.tobytes()
+
+        # decode access token
+        secret_key = pyseto.Key.new(version=4, purpose="public", key=pem_public_key)
+        decode = pyseto.decode(secret_key, token)
+        payload = decode.payload.decode('utf-8')
+
+        return json.loads(payload)
+        # try:
+        #     return jwt.decode(
+        #         token,
+        #         self.get_verifying_key(token),
+        #         algorithms=[self.algorithm],
+        #         audience=self.audience,
+        #         issuer=self.issuer,
+        #         leeway=self.get_leeway(),
+        #         options={
+        #             "verify_aud": self.audience is not None,
+        #             "verify_signature": verify,
+        #         },
+        #     )
+        # except InvalidAlgorithmError as ex:
+        #     raise TokenBackendError(_("Invalid algorithm specified")) from ex
+        # except InvalidTokenError as ex:
+        #     raise TokenBackendError(_("Token is invalid or expired")) from ex
